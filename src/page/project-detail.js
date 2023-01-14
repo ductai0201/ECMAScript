60ea12454007bf0e90650eb62f53fdded18be335
ductai0201 <ductai0201@gmail.com>
2023-01-14 09:24:04 +0700

    buoi 5

--- src/page/project-detail.js
+++ src/page/project-detail.js
@@ -2,15 +2,22 @@ import Header from "../components/header";
 import { projectFake } from "../data";
 
 const projectDetail = (id) => {
-
-    const currentProject = projectFake.find(project => 
-        project.id == id
-     );
-    console.log(currentProject);
-    if(!currentProject ) return 'Loading....' ;
-    return `
+  const currentProject = projectFake.find((project) => project.id == id);
+  console.log(currentProject);
+  if (!currentProject) return "Loading....";
+  return `
         ${Header()}
        <h1>${currentProject.name} </h1> 
+       ${
+         currentProject.teams
+           ? `<ul>${currentProject.teams
+               .map((member) => {
+                 return `${member.name}`;
+               })
+               .join(" ")} </ul>`
+           : ""
+       }  
+       
     `;
-}
-export default projectDetail ;
+};
+export default projectDetail;
