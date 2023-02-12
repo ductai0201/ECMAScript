72a8773974d2f2697ba3f52ab7e4c1d9c6690020
ductai0201 <ductai0201@gmail.com>
2023-02-12 09:56:36 +0700

    components

--- src/components/projectItem.js
+++ src/components/projectItem.js
@@ -1,8 +1,7 @@
+const projectItem = ({ project }) => {
+  console.log(project);
+  // if(!Array.isArray(project) || project.length == 0) return 0;
+  return `<div"><a href="/projectDetail/${project.id}"> ${project.name}</div>`;
+};
 
-const projectItem = ({project}) => {
-    // if(!Array.isArray(project) || project.length == 0) return 0; 
-  return (`<div"><a href="/projectDetail/${project.id}"> ${project.name}</div>`)
-    
-}
-
-export default projectItem
+export default projectItem;
--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,21 +1,19 @@
 import projectItem from "./projectItem";
 
 const projectList = ({ projects }) => {
-  // console.log(projects);
-  if(!Array.isArray(projects) || projects.length == 0) return null;
-  // return `
-  //       ${projects
-  //         ?.map((project) => {
-  //           return `${projectItem({project})}`;
-  //         })
-  //         .join("")}
-  //   `;
+  console.log(projects);
+  if (!Array.isArray(projects) || projects.length == 0) return null;
+  return `
+        ${projects
+          ?.map((project) => {
+            return `${projectItem({ project })}`;
+          })
+          .join("")}
+    `;
 
-  return projects && projects.length > 0
-    ? `${projects.map((project) => `<div><a href="/projectDetail/${project.id}">${project.name}</div>`).join("")}`
-    : '';
-
+  // return projects && projects.length > 0
+  //   ? `${projects.map((project) => `<div><a href="/projectDetail/${project.id}">${project.name}</div>`).join("")}`
+  //   : '';
 };
-
 
 export default projectList;
